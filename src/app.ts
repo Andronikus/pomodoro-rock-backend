1b51018eb45fd4374df483cc8d3bcd85f14b602c
Andronikus <tiagoafroque@gmail.com>
2020-10-17 16:50:16 +0100

    add bodyParser & dotenv to project

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+// middleware
+app.use(bodyParser.json());
+
+// routes
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'ok' });
 });
